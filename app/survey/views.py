902bd0ceb301c72a308efba2cabc443048a13c2b
Hannah Miller <mill6273@umn.edu>
2017-08-19 18:03:26 -0500

    default index page handling

--- app/survey/views.py
+++ app/survey/views.py
@@ -1,17 +1,30 @@
 # app/survey/views.py
 
 from flask import render_template
+from flask import redirect
 
 from . import survey
 from ..models import Survey
 
+
+# TODO uncomment!
+"""
+@survey.route('/')
+def route_to_website():
+    return redirect("http://z.umn.edu/emojistudy")
+"""
+
+    
 @survey.route('/<survey_id>')
-def landing_page(survey_id):
+def landing_page(survey_id=None):
     """
     Render the informed consent template on the / route
     TODO capture url id!
     """
-    Survey._survey_id = survey_id 
+    if survey_id == None:
+        Survey._survey_id = 1
+    else:
+        Survey._survey_id = survey_id
     return render_template('survey/index.html', informed_consent=Survey.informed_consent)
 
 @survey.route('/1')
